c4daab27c863d71a4f9c461d7e2e9473885af3b5
Nikita <nikovtb@mail.ru>
2016-02-25 14:39:36 +0300

    1.0.0
    lolvista Зацени

---
+++ Calc2/Calc2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            //string path = @"D:\Учеба\прога\2016\text\solvePlus.txt";
+            string path = @"D:\Учеба\прога\2016\text\solveAll2.txt";
+            //string path = @"D:\Учеба\прога\2016\text\solveClearBrackets.txt";
+
+            str = File.ReadAllText(path);
+            Console.WriteLine("Что считать?");
+            //Console.WriteLine(str);
+            Calc A = new Calc();
+            //Console.WriteLine(A.CountAll(str));
+            Console.WriteLine(A.Count(str));
+            ////double num = Convert.ToDouble("-1.5");
+            //double num = Double.Parse("-1,5");
+
+            Console.ReadLine();
+        }
+    }
+}
